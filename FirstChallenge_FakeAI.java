86a4cb8a5b6b918a14a46b69a4c5146dab463d78
BeThereOrBeSugaya <kigamail2000@yahoo.co.jp>
2017-10-23 15:20:28 +0900

    Update FirstChallenge_FakeAI.java

--- FirstChallenge_FakeAI.java
+++ FirstChallenge_FakeAI.java
@@ -1,116 +1,158 @@
-﻿import javax.swing.*;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
-public class SampleD8L1 {
-    private JFrame frame;
-    private JPanel panel;
-    private JLabel label;
-    private JButton btnA;
-    private JButton btnB;
-    private JButton btnC;
+public class FakeAiDoll extends Baseframe2 {
+//	JFrame frame;
 
+	Connection myCon = null;
+	Statement mySt = null;
 
-    // JButtonクラスのインスタンスを作成
-    //JRadioButton rdo1 = new JRadioButton();
-    //JRadioButton rdo2 = new JRadioButton();
-    //JRadioButton rdo3 = new JRadioButton();
+	JPanel panelAbove;
+	JPanel panelMiddle;
+	JLabel label;
+	JButton btnA;
+	JButton btnB;
+	JButton btnC;
+	JButton btnAction = new JButton("Action!");
+	ButtonGroup group:
+	JRadioButton[] radios;
 
-//  public JRadioButtonEvent() {
-//        ButtonGroup group = new ButtonGroup();
-//        Color[] colors = { Color.black, Color.red, Color.orange,
-//                           Color.yellow, Color.green, Color.cyan,
-//                           Color.blue, Color.magenta, Color.white
-//                         };
-//        JRadioButton[] radios = new JRadioButton[colors.length];
-//        for( int i=0; i<colors.length; i++ ) {
-//            radios[i] = new JRadioButton();
-//            radios[i].setBackground( colors[i] );
-//            radios[i].setText(i); 
-//            group.add( radios[i] );
-//            add( radios[i] );
+	// JLabelクラスのインスタンスを作成
+	//JLabel label = new JLabel("ラベルです");
 
-//        }
-//        radios[0].setForeground( Color.white );
-//        group.setSelected( radios[0].getModel(), true );
-//    }
- 
-
-    // JLabelクラスのインスタンスを作成
-  //  JLabel label = new JLabel("ラベルです");
-
-    // JTextFieldクラスのインスタンスを作成
-//    JTextField text = new JTextField("なんでそんなに怒ってるの？さぁ、言ってごらん…", 25);
-
-    // フレームにコンポーネントを追加
-    //frame.add(button);
+	// JTextFieldクラスのインスタンスを作成
+	JTextField text = new JTextField("なんでそんなに怒ってるの？さぁ、言ってごらん…", 25);
+   // フレームにコンポーネントを追加
+	//frame.add(button);
   //  frame.add(label);
-//    frame.add(text);
+//	frame.add(text);
 
 
-    public SampleD8L1() {
+	public FakeAiDoll() {
 
-        // フレームを作成してレイアウトをセット
-        frame = new JFrame("Click Me Buttons ！");
-        frame.setLayout(new BorderLayout());
+		// フレームを作成してレイアウトをセット
+		frame = new JFrame("Fake AI Doll");
+		frame.setLayout(new BorderLayout());
 
-        // パネルを作成してレイアウトをセット
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 3, 5, 5));
+		// パネルを作成してレイアウトをセット
+		panelAbove = new JPanel();
+		panelMiddle = new JPanel();
 
-        // 表示用ラベルを作成
-        label = new JLabel("リンナちゃんに慰めてもらいたい？ボタンを押してごらん。", JLabel.CENTER);
+		// 表示用ラベルを作成
+		label = new JLabel("リンナちゃんに慰めてもらいたい？ボタンを押してごらん。", JLabel.CENTER);
 
-        // btnAを作成
-        btnA = new JButton("btnA");
+		// btnAを作成
+		btnA = new JButton("Inprinting");
 
-        // btnAがクリックされた時のイベントを定義
-        btnA.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                label.setText("btnAがクリックされました");
-            }
-        });
 
-        // btnBを作成
-        btnB = new JButton("btnB");
 
-        // btnBがクリックされた時のイベントを定義
-        btnB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                label.setText("btnBがクリックされました");
-            }
-        });
+		// btnAがクリックされた時のイベントを定義
+		btnA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+			JRadioButtonEvent();
+			text.setText("AIに言葉を教えて！");
+			//label.setText("btnAがクリックされました");
+			}
+		});
 
-        // btnCを作成
-        btnC = new JButton("btnC");
 
-        // btnCがクリックされた時のイベントを定義
-        btnC.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                label.setText("btnCがクリックされました");
-            }
-        });
 
-        // パネルにボタンをセット
-        panel.add(btnA);
-        panel.add(btnB);
-        panel.add(btnC);
+		// btnBを作成
+		btnB = new JButton("btnB");
+		// btnBがクリックされた時のイベントを定義
+		btnB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				label.setText("btnBがクリックされました");
+			}
+		});
 
-        // フレームにパネルをセット（BorderLayoutの北側）
-        frame.add(panel, BorderLayout.NORTH);
+		// btnCを作成
+		btnC = new JButton("btnC");
+		// btnCがクリックされた時のイベントを定義
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				label.setText("btnCがクリックされました");
+			}
+		});
 
-        // フレームにラベルをセット（BorderLayoutの残りエリア）
-        //frame.add(label, BorderLayout.CENTER);//
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // フレームのサイズを設定して表示
-        frame.setSize(250, 100);
-        frame.setVisible(true);
-    }
+		panelAbove.setLayout(new GridLayout(1,3));
+		// パネルにボタンをセット
+		panelAbove.add(btnA);
+		panelAbove.add(btnB);
+		panelAbove.add(btnC);
 
-   // mainメソッドの定義
-    public static void main(String[] args) {
-        // SampleD8L1クラスの新規インスタンスを作成
-        SampleD8L1 application = new SampleD8L1();
-     }
+
+		// フレームにパネルをセット（BorderLayoutの北側と真ん中）
+		getContentPane().add(panelAbove, BorderLayout.NORTH);
+		getContentPane().add(panelMiddle, BorderLayout.CENTER);
+		getContentPane().add(btnAction, BorderLayout.SOUTH);
+
+		// フレームにラベルをセット（BorderLayoutの残りエリア）
+		//frame.add(label, BorderLayout.CENTER);//
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// フレームのサイズを設定して表示
+		frame.setSize(250, 100);
+		frame.setVisible(true);
+	}
+
+
+	public JRadioButtonEvent() {
+			group = new ButtonGroup();
+			radios = new JRadioButton[colors.length];
+
+			for( int i=0; i<10; i++ ) {
+			radios[i] = new JRadioButton();
+			radios[i].setText(i);
+			group.add( radios[i] );
+			add( radios[i] );
+			}
+
+		radios[0].setForeground( Color.white );
+		group.setSelected( radios[0].getModel(), true );
+	}
+
+
+//resultSetで昇順・降順の両方可能な奴
+//Statement stmt = con.createStatement(
+//					   ResultSet.TYPE_SCROLL_INSENSITIVE, 
+//					   ResultSet.CONCUR_READ_ONLY);
+//ResultSet scrollingRS = stmt.executeQuery("SELECT * FROM whatever");
+
+	void showMyMessage(String strMessage) {
+		JOptionPane.showMessageDialog(this, strMessage);
+	}
+
+	void doUpdate(String sql,int a,int b) {
+
+		int AiF;
+
+		AiF = (a*b);
+
+		try {
+		mySt = myCon.createStatement() ;
+
+		ResultSet myRs = mySt.executeQuery("SELECT * FROM HOGEHOGE WHERE KIBUN == "+AiF);
+	
+		while(myRs.next){
+			String line = myRs.getString("NAME") ;
+			String strMes;
+		 	strMes.append(line + "\n");
+
+		}
+	
+	JOptionPane.showMessageDialog(this, strMes);
+
+	//intCount = mySt.executeUpdate(sql);
+	}catch(Exception ex) {
+		showMyMessage("doUpdateでエラー発生：" + ex);
+		intCount = 0;
+	}
+		return intCount;
+	}
+
+
 }
